354b1b6560e4229c8c6f79e6ccb73dcfbf4a5a48
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-02 09:08:04 +1100

    - изменена схема работы с binding во фрагментах

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragment/FavoritesFragment.kt
@@ -6,17 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
+import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
 class FavoritesFragment: Fragment() {
 
-    private lateinit var binding: FragmentFavoritesBinding
+    private var _binding: FragmentFavoritesBinding? =null
+    private val binding get()= _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchFragment.kt
@@ -9,14 +9,20 @@ import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment() {
 
-    private lateinit var binding: FragmentSearchBinding
+    private var _binding: FragmentSearchBinding? =null
+    private val binding get()= _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
