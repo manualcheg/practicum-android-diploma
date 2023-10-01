40fbe622c8708336ac04d83dcede63e2ae97fa80
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-01 17:52:12 +1100

    - добавил новые иконки главного меню, старые удалил - набросал нижнее меню и навигацию - исправил тему в манифесте на тему приложения - добавил tools:ignore="LockedOrientationActivity"

--- app/src/main/java/ru/practicum/android/diploma/app/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/RootActivity.kt
@@ -2,13 +2,28 @@ package ru.practicum.android.diploma.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigationView: BottomNavigationView = binding.navigationView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navigationView.setupWithNavController(navController)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
