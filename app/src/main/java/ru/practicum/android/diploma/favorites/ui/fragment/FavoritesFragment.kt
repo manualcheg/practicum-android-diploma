92ecd49868da8bfc5dcd3b395b4a4dba52df5cb6
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-02 09:08:59 +1100

    - удалены неиспользуемые импорты

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/fragment/FavoritesFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
-import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
 class FavoritesFragment: Fragment() {
 
