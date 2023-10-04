036b5c400f3535ca0e0838b80bf0767d97cba837
Vladislav <bigmag174@yandex.ru>
2023-10-04 21:12:15 +0500

    добавил модели

---
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/data/model/dto/AreaDto.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.vacancy.data.model.dto
+
+data class AreaDto(
+    val id: String, //Идентификатор региона из справочника
+    val name: String, //Название региона
+)
