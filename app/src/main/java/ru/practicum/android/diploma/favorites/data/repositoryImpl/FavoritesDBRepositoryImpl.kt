41d5d2784d6c64d9abbde0b2682e997a2b5b315c
KochubeevAndrei <manualcheg@yandex.ru>
2023-10-04 22:36:45 +0300

    1. Добавлен интерфейс общения с БД 2. Добавлена его реализация FavoritesStorageImpl 3. Изменена реализация FavoritesDBRepositoryImpl для использования экземпляра FavoriteStorage вместо экземпляра БД

--- app/src/main/java/ru/practicum/android/diploma/favorites/data/repositoryImpl/FavoritesDBRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/repositoryImpl/FavoritesDBRepositoryImpl.kt
@@ -3,33 +3,26 @@ package ru.practicum.android.diploma.favorites.data.repositoryImpl
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import ru.practicum.android.diploma.common.domain.model.vacancy_models.Vacancy
-import ru.practicum.android.diploma.favorites.data.db.AppDataBase
-import ru.practicum.android.diploma.favorites.data.db.FavoritesVacancyEntity
-import ru.practicum.android.diploma.favorites.data.mapper.VacancyDbConverter
+import ru.practicum.android.diploma.favorites.data.dataSource.FavoritesStorage
 import ru.practicum.android.diploma.favorites.domain.repository.FavoritesDBRepository
 
 class FavoritesDBRepositoryImpl(
-    private val appDataBase: AppDataBase,
-    private val vacancyDbConverter: VacancyDbConverter
+    private val favoritesStorage: FavoritesStorage,
 ) : FavoritesDBRepository {
     override suspend fun addToFavorites(vacancy: Vacancy) {
-        appDataBase.FavoritesVacanciesDao().addToFavorites(vacancyDbConverter.map(vacancy))
+        favoritesStorage.addToFavorites(vacancy)
     }
 
     override suspend fun deleteFromFavorites(id: Int) {
-        appDataBase.FavoritesVacanciesDao().deleteFromFavorites(id)
+        favoritesStorage.deleteFromFavorites(id)
     }
 
     override suspend fun getFavorites(): Flow<List<Vacancy>> = flow {
-        val listOfVacancies = appDataBase.FavoritesVacanciesDao().getFavorites()
-        emit(mapList(listOfVacancies))
+        val listOfVacancies = favoritesStorage.getFavorites()
+        emit(listOfVacancies)
     }
 
     override suspend fun getVacancy(id: Int): Vacancy {
-        return vacancyDbConverter.map(appDataBase.FavoritesVacanciesDao().getVacancy(id))
-    }
-
-    private fun mapList(favouritesList: List<FavoritesVacancyEntity>): List<Vacancy> {
-        return favouritesList.map { track -> vacancyDbConverter.map(track)}
+        return favoritesStorage.getVacancy(id)
     }
 }
