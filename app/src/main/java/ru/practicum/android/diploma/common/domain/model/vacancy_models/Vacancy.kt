a2ae7efb100543c1efaaa95e9518a3525487d6bc
Ilnur Valiev <judjingm@gmail.com>
2023-10-04 22:01:11 +0400

    Refactoring #1. оправил dao и entity согласно ответу запроса от апи

--- app/src/main/java/ru/practicum/android/diploma/common/domain/model/vacancy_models/Vacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/model/vacancy_models/Vacancy.kt
@@ -2,13 +2,13 @@ package ru.practicum.android.diploma.common.domain.model.vacancy_models
 data class Vacancy(
     val id: Int,
     val name: String, // "name": "Секретарь",
-    val area: Area,
+    val area: Area?,
     val employer: Employer?, // Информация о компании работодателя
     val salary: Salary?, // Зарплата
     val experience: Experience?, // Опыт работы ("От 1 года до 3 лет")
     val employment: Employment?, // Тип занятости ("Полная занятость")
-    val description: String, // string html
-    val keySkills: List<KeySkill>, // (Ключевые навыки) Список ключевых навыков, не более 30
+    val description: String?, // string html
+    val keySkills: List<KeySkill>?, // (Ключевые навыки) Список ключевых навыков, не более 30
     val schedule: Schedule?, // График работы (Удаленныая работа)
     val contacts: Contacts?,
 )
