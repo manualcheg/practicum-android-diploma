59cfeab12991d424c3a0844a1f7443acb282bb44
Vladislav <bigmag174@yandex.ru>
2023-10-05 15:54:38 +0500

    Удалил

--- app/src/main/java/ru/practicum/android/diploma/vacancy/domain/useCase/LoadVacancyFromDatabaseUseCase.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.vacancy.domain.useCase
-
-import ru.practicum.android.diploma.common.domain.model.vacancy_models.Vacancy
-
-interface LoadVacancyFromDatabaseUseCase {
-    suspend fun getVacancyFromDataBaseById(vacancyId: Int): Vacancy
-}
