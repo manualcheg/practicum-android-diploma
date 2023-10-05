e599a39a3cfc5e1f973b7b9e9a499cfef933e65f
Ilnur Valiev <judjingm@gmail.com>
2023-10-05 22:44:04 +0400

    Создал 2 новые модели для ui слоя. Создал к ним маппер

---
+++ app/src/main/java/ru/practicum/android/diploma/common/ui/model/PhoneUi.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.common.ui.model
+
+data class PhoneUi (
+    val formattedNumber: String,
+    val city: String,
+    val comment: String,
+    val country: String,
+    val number: String
+)
