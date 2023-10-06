85752d4f827751aaff2a553a835d7233b5d42fd6
KochubeevAndrei <manualcheg@yandex.ru>
2023-10-06 20:46:07 +0300

    1. Отмена Промежуточное состояние 2

--- app/src/main/java/ru/practicum/android/diploma/favorites/data/db/AppDataBase.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/db/AppDataBase.kt
@@ -5,8 +5,8 @@ import androidx.room.RoomDatabase
 
 @Database(
     version = 1,
-    entities = [FavoritesVacancyEntity::class, AreaEntity::class, EmployerEntity::class, SalaryEntity::class, ExperienceEntity::class, EmploymentEntity::class, KeySkillEntity::class, ScheduleEntity::class, ContactsEntity::class]
+    entities = [FavoritesVacancyEntity::class]
 )
-abstract class AppDataBase : RoomDatabase() {
+abstract class AppDataBase: RoomDatabase() {
     abstract fun FavoritesVacanciesDao(): FavoritesVacanciesDao
 }
