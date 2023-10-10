99ec26244987344bdb79fe851d548a0705108374
Ilnur Valiev <judjingm@gmail.com>
2023-10-10 21:53:05 +0400

    Поправил проблему c запросом "it 32", с сервера приходят null там где не должны быть null.

--- app/src/main/java/ru/practicum/android/diploma/search/data/model/dto/LogoUrlsDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/model/dto/LogoUrlsDto.kt
@@ -6,6 +6,6 @@ data class LogoUrlsDto(
     @SerializedName("240")
     val logo240: String?, // URL логотипа с размером менее 240px по меньшей стороне
     @SerializedName("90")
-    val logo90: String, // URL логотипа с размером менее 90px по меньшей стороне
-    val original: String // URL необработанного логотипа
+    val logo90: String?, // URL логотипа с размером менее 90px по меньшей стороне
+    val original: String? // URL необработанного логотипа
 )
