fdd6d74b3642b02481a3c24cb37254d11a82d1d3
KochubeevAndrei <manualcheg@yandex.ru>
2023-10-14 10:52:39 +0300

    1. Созданы в классе LocalCache функции добавления значений фильтров

--- app/src/main/java/ru/practicum/android/diploma/filter/data/dataSource/FilterOptionsDataSource.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/dataSource/FilterOptionsDataSource.kt
@@ -1,9 +1,17 @@
 package ru.practicum.android.diploma.filter.data.dataSource
 
+import ru.practicum.android.diploma.filter.data.model.dto.Area
+import ru.practicum.android.diploma.filter.data.model.dto.Country
 import ru.practicum.android.diploma.filter.data.model.dto.Filter
+import ru.practicum.android.diploma.filter.data.model.dto.Industry
 
 interface FilterOptionsDataSource {
     fun getFilterOptions(): Filter?
     fun putFilterOptions(options: Filter)
+    fun addCountry(country: Country)
+    fun addArea(area: Area)
+    fun addIndustry(industry: Industry)
+    fun addSalary(salary: Int)
+    fun addOnlyWithSalary(option: Boolean)
     fun clearFilterOptions()
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/data/dataSourceImpl/FilterOptionsDataSourceImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/dataSourceImpl/FilterOptionsDataSourceImpl.kt
@@ -3,7 +3,10 @@ package ru.practicum.android.diploma.filter.data.dataSourceImpl
 import ru.practicum.android.diploma.filter.data.dataSource.FilterOptionsDataSource
 import ru.practicum.android.diploma.filter.data.db.FilterDb
 import ru.practicum.android.diploma.filter.data.db.LocalCache
+import ru.practicum.android.diploma.filter.data.model.dto.Area
+import ru.practicum.android.diploma.filter.data.model.dto.Country
 import ru.practicum.android.diploma.filter.data.model.dto.Filter
+import ru.practicum.android.diploma.filter.data.model.dto.Industry
 
 class FilterOptionsDataSourceImpl(
     private val filterDb: FilterDb,
@@ -15,11 +18,30 @@ class FilterOptionsDataSourceImpl(
         } else {
             localCache.getFilterCache()
         }
-
     }
 
     override fun putFilterOptions(options: Filter) {
         filterDb.putFilterOptions(options)
+    }
+
+    override fun addCountry(country: Country) {
+        localCache.addCountry(country)
+    }
+
+    override fun addArea(area: Area) {
+        localCache.addArea(area)
+    }
+
+    override fun addIndustry(industry: Industry) {
+        localCache.addIndustry(industry)
+    }
+
+    override fun addSalary(salary: Int) {
+        localCache.addSalary(salary)
+    }
+
+    override fun addOnlyWithSalary(option: Boolean) {
+        localCache.addOnlyWithSalary(option)
     }
 
     override fun clearFilterOptions() {
