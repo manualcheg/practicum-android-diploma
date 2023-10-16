396552b6a299594722b6fad32bf82120d04a4a65
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-16 21:59:49 +1100

    - bugfix merge in dev

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/CountryViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/CountryViewHolder.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.filter.ui
 
 import androidx.recyclerview.widget.RecyclerView
-import ru.practicum.android.diploma.databinding.ItemCountryBinding
+import ru.practicum.android.diploma.databinding.ItemCountryAndRegionBinding
 import ru.practicum.android.diploma.filter.ui.model.CountryUi
 
 class CountryViewHolder(
-    private val binding: ItemCountryBinding,
+    private val binding: ItemCountryAndRegionBinding,
     private val onSelectCountryButtonClicked: (CountryUi) -> Unit
 ) :
     RecyclerView.ViewHolder(binding.root) {
