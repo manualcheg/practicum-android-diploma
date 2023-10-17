2af90c70a1f4854b9b69ec4ba0edefc290cee0a0
Ilnur Valiev <judjingm@gmail.com>
2023-10-17 14:38:04 +0400

    Preparing for merge #1

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/FilteringSettingsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/FilteringSettingsViewModel.kt
@@ -4,21 +4,11 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.filter.domain.useCase.AddSalaryFilterUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.AddOnlyWithSalaryFilterUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.ClearFilterOptionsUseCase
 import ru.practicum.android.diploma.filter.domain.useCase.GetAreasUseCase
 import ru.practicum.android.diploma.filter.domain.useCase.GetCountriesUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.GetFilterOptionsUseCase
 import ru.practicum.android.diploma.filter.domain.useCase.GetIndustriesUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.PutFilterOptionsUseCase
 
 class FilteringSettingsViewModel(
-    private val addSalaryFilterUseCase: AddSalaryFilterUseCase,
-    private val addOnlyWithSalaryFilterUseCase: AddOnlyWithSalaryFilterUseCase,
-    private val clearFilterOptionsUseCase: ClearFilterOptionsUseCase,
-    private val getFilterOptionsUseCase: GetFilterOptionsUseCase,
-    private val putFilterOptionsUseCase: PutFilterOptionsUseCase,
     private val getAreasUseCase: GetAreasUseCase,
     private val getCountriesUseCase: GetCountriesUseCase,
     private val getIndustriesUseCase: GetIndustriesUseCase,
--- app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/SelectRegionAndSectorViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/SelectRegionAndSectorViewModel.kt
@@ -1,19 +1,5 @@
 package ru.practicum.android.diploma.filter.ui.viewModel
 
 import androidx.lifecycle.ViewModel
-import ru.practicum.android.diploma.filter.domain.useCase.AddAreaFilterUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.AddCountryFilterUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.AddIndustryFilterUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.GetAreaListUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.GetCountryListUseCase
-import ru.practicum.android.diploma.filter.domain.useCase.GetIndustryListUseCase
 
-class SelectRegionAndSectorViewModel(
-    private val addAreaFilterUseCase: AddAreaFilterUseCase,
-    private val addCountryFilterUseCase: AddCountryFilterUseCase,
-    private val addIndustryFilterUseCase: AddIndustryFilterUseCase,
-    private val getAreaListUseCase: GetAreaListUseCase,
-    private val getCountryListUseCase: GetCountryListUseCase,
-    private val getIndustryListUseCase: GetIndustryListUseCase,
-) : ViewModel() {
-}
+class SelectRegionAndSectorViewModel : ViewModel()
