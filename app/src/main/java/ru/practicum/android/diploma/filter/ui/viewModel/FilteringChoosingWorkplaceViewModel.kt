e5f101b0d0aaad7a99dc0ba103f75870bfddcc29
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-19 16:02:00 +1100

    - добавлен фрагмент и viewModel для экрана выбора места работы

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/FilteringChoosingWorkplaceViewModel.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.filter.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class FilteringChoosingWorkplaceViewModel : ViewModel() {
+
+    init{
+
+    }
+}
