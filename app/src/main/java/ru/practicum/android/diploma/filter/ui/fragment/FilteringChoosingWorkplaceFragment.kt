f4ebfc9552ad470a1a1f393e552d68d23e6ac3c6
Vladislav <bigmag174@yandex.ru>
2023-10-19 17:30:44 +0500

    Добавил обработку состояний для полей страна и регион, Добавил кнопку Выбрать

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FilteringChoosingWorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/fragment/FilteringChoosingWorkplaceFragment.kt
@@ -5,17 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFilteringChoosingWorkplaceBinding
+import ru.practicum.android.diploma.filter.ui.model.FilterFieldsState
 import ru.practicum.android.diploma.filter.ui.viewModel.FilteringChoosingWorkplaceViewModel
 
 class FilteringChoosingWorkplaceFragment : Fragment() {
 
+    private val viewModel: FilteringChoosingWorkplaceViewModel by viewModel()
+
     private var _binding: FragmentFilteringChoosingWorkplaceBinding? = null
     private val binding get() = _binding!!
-
-    private val viewModel by viewModel<FilteringChoosingWorkplaceViewModel>()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,8 +26,62 @@ class FilteringChoosingWorkplaceFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.countryState.observe(viewLifecycleOwner) {
+            renderCountryState(it)
+        }
+        viewModel.regionState.observe(viewLifecycleOwner) {
+            renderRegionState(it)
+        }
+
+        setOnClickListeners()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadFilterOptions()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun setOnClickListeners() {
+
+        binding.choosingWorkplaceToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
+//        binding.choosingWorkplaceSelectButtonTextView.setOnClickListener {
+//            viewModel.addAreaFilter()
+//        }
+    }
+
+
+    private fun renderCountryState(state: FilterFieldsState) {
+        when (state) {
+            is FilterFieldsState.Empty -> {
+                binding.selectedCountryTextInputEditText.text?.clear()
+            }
+
+            is FilterFieldsState.Content -> {
+                binding.selectedCountryTextInputEditText.setText(state.text)
+            }
+        }
+    }
+
+    private fun renderRegionState(state: FilterFieldsState) {
+        when (state) {
+            is FilterFieldsState.Empty -> {
+                binding.selectedRegionTextInputEditText.text?.clear()
+            }
+
+            is FilterFieldsState.Content -> {
+                binding.selectedCountryTextInputEditText.setText(state.text)
+            }
+        }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/FilteringChoosingWorkplaceViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/viewModel/FilteringChoosingWorkplaceViewModel.kt
@@ -1,10 +1,79 @@
 package ru.practicum.android.diploma.filter.ui.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.common.domain.model.filter_models.AreaFilter
+import ru.practicum.android.diploma.common.domain.model.filter_models.CountryFilter
+import ru.practicum.android.diploma.common.domain.model.filter_models.Filter
+import ru.practicum.android.diploma.filter.domain.useCase.AddAreaFilterUseCase
+import ru.practicum.android.diploma.filter.domain.useCase.AddCountryFilterUseCase
+import ru.practicum.android.diploma.filter.domain.useCase.GetFilterOptionsUseCase
+import ru.practicum.android.diploma.filter.ui.mapper.FilterDomainToFilterUiConverter
+import ru.practicum.android.diploma.filter.ui.model.FilterFieldsState
 
-class FilteringChoosingWorkplaceViewModel : ViewModel() {
+class FilteringChoosingWorkplaceViewModel(
+    private val addAreaFilterUseCase: AddAreaFilterUseCase,
+    private val addCountryFilterUseCase: AddCountryFilterUseCase,
+    private val getFilterOptionsUseCase: GetFilterOptionsUseCase,
+    private val filterDomainToFilterUiConverter: FilterDomainToFilterUiConverter,
+) : ViewModel() {
 
-    init{
+    private val _countryState = MutableLiveData<FilterFieldsState>()
+    val countryState: LiveData<FilterFieldsState> = _countryState
 
+    private val _regionState = MutableLiveData<FilterFieldsState>()
+    val regionState: LiveData<FilterFieldsState> = _regionState
+
+    init {
+        loadFilterOptions()
+    }
+
+    fun loadFilterOptions() {
+        val filter: Filter? = getFilterOptionsUseCase.execute()
+        if (filter != null) {
+            if (filter.country != null) {
+                setCountryState(
+                    FilterFieldsState.Content(
+                        filterDomainToFilterUiConverter.mapFilterToFilterUi(
+                            filter
+                        ).countryName
+                    )
+                )
+            } else {
+                setCountryState(
+                    FilterFieldsState.Empty
+                )
+            }
+
+            if (filter.area != null) {
+                setRegionState(
+                    FilterFieldsState.Content(
+                        filterDomainToFilterUiConverter.mapFilterToFilterUi(
+                            filter
+                        ).areaName
+                    )
+                )
+            } else {
+                setRegionState(FilterFieldsState.Empty)
+            }
+        }
+    }
+
+    fun addAreaFilter(area: AreaFilter) {
+        addAreaFilterUseCase.execute(area)
+    }
+
+    fun addCountryFilter(country: CountryFilter) {
+        addCountryFilterUseCase.execute(country)
+    }
+
+
+    private fun setCountryState(state: FilterFieldsState) {
+        _countryState.value = state
+    }
+
+    private fun setRegionState(state: FilterFieldsState) {
+        _regionState.value = state
     }
 }
