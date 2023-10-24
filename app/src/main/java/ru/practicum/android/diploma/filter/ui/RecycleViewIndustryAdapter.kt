e8b5b0562617b943043a9090af0a5fb648b514ba
Ilnur Valiev <judjingm@gmail.com>
2023-10-24 15:13:37 +0400

    Fixed merge

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/RecycleViewIndustryAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/RecycleViewIndustryAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.ItemSectorBinding
+import ru.practicum.android.diploma.filter.DiffCallbackIndustry
 import ru.practicum.android.diploma.filter.ui.model.IndustryUi
 
 class RecycleViewIndustryAdapter(
