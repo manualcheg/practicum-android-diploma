e888215d0fc4c776835dd0f48f4a9a499e59b340
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-28 17:58:11 +1100

    - отформатирован код в Areas

--- app/src/main/java/ru/practicum/android/diploma/common/domain/model/filter_models/Areas.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/model/filter_models/Areas.kt
@@ -1,5 +1,3 @@
 package ru.practicum.android.diploma.common.domain.model.filter_models
 
-data class Areas(
-    val arealList: List<AreaFilter>,
-)
+data class Areas(val arealList: List<AreaFilter>)
