1455a8ddfea78f135f5b57d9e911193e8b9417a9
Evgeniy Shurygin <anonimno@gmail.com>
2023-10-28 18:06:31 +1100

    - отформатирован код в ClearTempFilterOptionsUseCaseImpl

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/useCase/ClearTempFilterOptionsUseCaseImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/useCase/ClearTempFilterOptionsUseCaseImpl.kt
@@ -2,8 +2,9 @@ package ru.practicum.android.diploma.filter.domain.useCase
 
 import ru.practicum.android.diploma.filter.domain.repository.FiltersRepository
 
-class ClearTempFilterOptionsUseCaseImpl(private val filtersRepository: FiltersRepository) :
-    ClearTempFilterOptionsUseCase {
+class ClearTempFilterOptionsUseCaseImpl(
+    private val filtersRepository: FiltersRepository
+) : ClearTempFilterOptionsUseCase {
     override fun execute() {
         filtersRepository.clearTempFilterOptions()
     }
